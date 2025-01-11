8a05d588f3403159b8f37983cb600f0df5644063
Charles Villalpando <charlesvill@g.ucla.edu>
2025-01-10 18:41:39 -0800

    Pool as a connection string w/ ssl settings included in string

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,5 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 module.exports = new Pool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER ,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT 
+  connectionString: process.env.CONNECTION_STRING
 });
